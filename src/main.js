330efb651b4651e8c53135bd5f21e24e0aca52cf
Zach Krogh <zakrogh@gmail.com>
2019-09-16 21:49:37 -0700

    refactor main script to include async and await

--- src/main.js
+++ src/main.js
@@ -3,24 +3,30 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-$(document).ready(function(){
-  // let promise = new Promise(function(resolve, reject) {
-  //   let request = new XMLHttpRequest();
-  //   let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&skip=0&limit=10&user_key=${process.env.API_KEY}`;
-  //   request.onload = function() {
-  //     if (this.status === 200) {
-  //       resolve(request.response);
-  //     } else {
-  //       reject(Error(request.statusText));
-  //     }
-  //   }
-  //   request.open("GET", url, true);
-  //   request.send();
-  // });
-  // promise.then(function(response) {
-  //   let body = JSON.parse(response);
-  //   console.log(body);
-  // }, function(error) {
-  //   console.log("error");
-  // });
+const callApi = async function(){
+  let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&skip=0&limit=10&user_key=${process.env.API_KEY}`;
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText));
+      }
+    }
+    request.open("GET", url, true);
+    request.send();
+  });
+  let apiData = promise.then(async function(response) {
+    let content = JSON.parse(response);
+    return content;
+  }, function(error) {
+    console.log("error");
+  });
+  return apiData;
+}
+
+$(document).ready(async function(){
+  let data = await callApi();
+  console.log(data);
 });
