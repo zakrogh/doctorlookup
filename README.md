4b53a67cca8eee710f900bb2799890fb06a7822a
Zach Krogh <zakrogh@gmail.com>
2019-09-17 01:22:45 -0700

    finalize ui and api calls

---
+++ README.md
@@ -0,0 +1,37 @@
+# _Doctor Lookup_
+
+#### _A site created to practice API calls 9/17/2019_
+
+#### By _Zach Krogh_
+
+## Description
+
+_This site asks the user for a Doctor's name and/or a medical issue (such as cancer), it then returns a list of doctors with practices in the Seattle area who treat the given issue. The list of doctors will contain the inputted doctor name, although it can include their middle name._
+
+## Setup/Installation Requirements
+
+* _Download all files into the same directory and run "npm install" in the terminal from the project directory_
+* _Then run "npm run build" in the terminal from the project directory_
+* _Open the index.html file in the "dist" directory_
+* _Alternatively, run "npm run start" instead of "npm run build" and a browser window will open_
+
+## Project Specifications
+
+* _The program returns a list of doctors with practices in the Seattle area_
+* _The program accepts a doctor name and returns a list of doctors with that name_
+* _The program accepts a medical issue and returns a list of doctors who mention that medical issue_
+
+
+## Support and contact details
+
+_contact: zakrogh@gmail.com_
+
+## Technologies Used
+
+_HTML, CSS with Bootstrap, JavaScript with JQuery_
+
+### License
+
+*MIT License*
+
+Copyright (c) 2019 **_Zach Krogh_**
--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-const callApi = async function(){
+const callApi = async function(queryInput, nameInput){
+  let query = queryInput;
+  let name = nameInput;
   let promise = new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&skip=0&limit=10&user_key=${process.env.API_KEY}`;
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&skip=0&limit=10&user_key=${process.env.API_KEY}&query=${query}&name=${name}`;
     request.onload = function() {
       if (this.status === 200) {
         resolve(request.response);
@@ -16,17 +18,45 @@ const callApi = async function(){
     }
     request.open("GET", url, true);
     request.send();
+
   });
   let apiData = promise.then(async function(response) {
     let content = JSON.parse(response);
     return content;
-  }, function(error) {
-    console.log("error");
+  }, async function(error) {
+    return error;
   });
   return apiData;
 }
-
-$(document).ready(async function(){
-  let data = await callApi();
-  console.log(data);
+const displayData = function(data){
+  $(".output").text("");
+  if(data.data.length === 0)
+    $(".output").text("No results found.");
+  for(let i = 0; i < data.data.length; i++){
+    $(".output").append(data.data[i].profile.first_name + " " + data.data[i].profile.last_name + "<br>");
+    $(".output").append(data.data[i].practices[0].visit_address.street + ", " + data.data[i].practices[0].visit_address.city + ", " + data.data[i].practices[0].visit_address.state + " " + data.data[i].practices[0].visit_address.zip + "<br>");
+    $(".output").append("Phone number: " + data.data[i].practices[0].phones[0].number + "<br>");
+    let newPatients = data.data[i].practices[0].accepts_new_patients ? 'Yes' : 'No';
+    $(".output").append("Accepting new patients: " + newPatients);
+    $(".output").append("<br><hr>");
+  }
+}
+const displayError = function(data){
+  $(".output").text(data);
+}
+//I think I might not be using promises correctly, because I need
+// async in my function declarations and await in my function calls
+// otherwise my functions return promise objects who's properties
+// cannot be referenced
+$(document).ready(function(){
+  $(".mainform").submit(async function(event){
+    event.preventDefault();
+    let query = $("#medicalissue").val();
+    let name = $("#doctorname").val();
+    let data = await callApi(query,name);
+    if(data.data)
+      displayData(data);
+    else
+      displayError(data);
+  });
 });
